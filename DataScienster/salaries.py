9083059cf687ccd746b3d29907e818502ee99479
R2 D2 <p234a137@gmail.com>
2017-03-08 21:31:42 -0800

    completed salaries

--- DataScienster/salaries.py
+++ DataScienster/salaries.py
@@ -11,6 +11,8 @@ salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
 print(len(salaries_and_tenures))
 pprint.pprint(salaries_and_tenures)
 
+
+# Make a plot
 """ tried with bashplotlib without success
 with open('/tmp/tmp1', 'w') as f:
     for item in salaries_and_tenures:
@@ -24,11 +26,54 @@ from bashplotlib.scatterplot import plot_scatter
 #    plot_scatter(f)
 plot_scatter('/tmp/tmp1')
 """
-
+# use matplotlib
 import matplotlib.pyplot as plt
-
 y,x = zip(*salaries_and_tenures)
 fig, ax = plt.subplots( nrows=1, ncols=1  )  # create figure & 1 axis
 ax.scatter(x, y)
 fig.savefig('sal1.png')   # save the figure to file
 plt.close(fig)    # close the figure
+
+
+# keys are years, values are lists of the salaries for each tenure
+from collections import defaultdict
+salary_by_tenure = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+    salary_by_tenure[tenure].append(salary)
+    
+# keys are years, each value is the average salary for that tenure
+average_salary_by_tenure = {
+        tenure: sum(salaries) / len(salaries)
+        for tenure, salaries in salary_by_tenure.items()
+        }
+
+print("\nAverage Salary by Tenure Year:")
+pprint.pprint(average_salary_by_tenure)
+
+
+# It is probably better to bucket the tenures
+def tenure_bucket(tenure):
+    if tenure < 2:
+        return "less than two"
+    elif tenure < 5:
+        return "between two and five"
+    else:
+        return "more than five"
+
+
+# keys are tenure buckets, values are lists of salaries for that bucket
+salary_by_tenure_bucket = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+    bucket = tenure_bucket(tenure)
+    salary_by_tenure_bucket[bucket].append(salary)
+
+# keys are tenure buckets, values are average salary for that bucket
+average_salary_by_tenure_bucket = {
+        tenure_bucket: sum(salaries) / len(salaries)
+        for tenure_bucket, salaries in salary_by_tenure_bucket.items()
+        }
+
+print("\nAverage Salary by Tenure Bucket:")
+pprint.pprint(average_salary_by_tenure_bucket)
