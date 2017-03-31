19d845fcc7c14f803625f15090696d399b5ca178
R2 D2 <p234a137@gmail.com>
2017-03-30 17:44:40 -0700

    list comprehension

--- DataScienster/list_comprehension.py
+++ DataScienster/list_comprehension.py
@@ -26,3 +26,20 @@ pprint.pprint(square_set)
 zeroes = [0 for _ in even_numbers] # use _ if you do not need the value from the list
 print("zeroes")
 pprint.pprint(zeroes)
+
+
+# A list comprehension can include multiple for-s
+# and later for-s can use the results of earlier ones
+pairs = [ (x, y)
+        for x in range(3)
+        for y in range(3)
+        ]
+print("pairs")
+pprint.pprint(pairs)
+
+increasing_pairs = [(x, y)        # only pairs with x < y
+        for x in range(4)        # range(lo, hi) equals
+        for y in range(x+1, 4)   # [lo, lo+1, ..., hi-1]
+        ]
+print("increasing pairs")
+pprint.pprint(increasing_pairs)
